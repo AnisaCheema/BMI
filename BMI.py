9d592f9976113a27a81f3864d79eafbff3e441a5
AnisaCheema <82442783+AnisaCheema@users.noreply.github.com>
2021-06-17 10:27:09 +0200

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,5 +1,5 @@
 Based on the height and weight of person, write a python script that will calculate BMI (Body Mass Index). 
-Ask a user to enter height and weight and store the answer in a vaiable:
+
 #weight / height * height
 #Print out the BMI to the user
 
