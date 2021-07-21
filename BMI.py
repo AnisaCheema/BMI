08a49a68ac7c28301f1f0530444aed0e40f6bc11
AnisaCheema <82442783+AnisaCheema@users.noreply.github.com>
2021-07-21 23:22:28 +0200

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,18 +1,26 @@
-BMI (Body Mass Index), based on height and weight of a person. 
-user_name = input("Enter your name: ")
-user_age = input("Enter your age: ")
+BMI calculator, based on height, weight and gender of a person. 
 
-height = float(input("Enter your Hight in meters: "))
-weight = float(input("Enter your weight in kilos: "))
+user_name=input("Enter your name: ")
+user_gender=input("For female, enter F: otherwise enter M: ")
+
+height=float(input("Enter your Hight in meters: "))
+weight=float(input("Enter your weight in kilos: "))
   
 bmi = weight / height **2 
 
-if bmi < 19.5:
-  print (f " {user_name} you are {user_age} years old. Your body mas is {bmi}. you are underweight. ")
-    
-elif bmi >= 19.5 and bmi <= 26: 
-  print(f " {user_name} you are {user_age} years old. Your body mas is {bmi}.you are healthy. ")
-    
-elif bmi > 26: 
-  print(f " {user_name} you are {user_age} years old. Your body mas is {bmi}. you are overweight. ")
+if user_gender == "F":
+  if bmi < 19.5:
+    print (f"{user_name}. Your body mas is {bmi}. You are underweight")
+  elif bmi >= 19.5 and bmi <= 26: 
+    print(f"{user_name}. Your body mas is {bmi}. You are healthy")   
+  elif bmi > 26: 
+    print(f" {user_name}. Your body mas is {bmi}. You are overweight")
 
+if user_gender == "M":
+  if bmi < 18.5:
+    print (f"{user_name}. Your body mas is {bmi}. You are underweight")
+  elif bmi >= 18.5 and bmi <= 25: 
+    print(f"{user_name}. Your body mas is {bmi}. You are healthy")   
+  elif bmi > 25:
+    print(f"{user_name}. Your body mas is {bmi}. You are over-weight.")
+print("\n\n\t\t! programe is finished !")
